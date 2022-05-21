bb5878e9e61542f9b3ae4d0a9fa8615f5d379292
Juliano <julianoferreira336@gmail.com>
2022-05-21 20:53:51 -0300

    Initial commit

---
+++ src/main/java/com/s2_creche/S2CrecheApplication.java
@@ -0,0 +1,13 @@
+package com.s2_creche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2CrecheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S2CrecheApplication.class, args);
+	}
+
+}
